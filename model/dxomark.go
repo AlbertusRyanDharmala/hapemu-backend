e9e3d7ae982f25bc20c424c8787266713b1ecb08
Hunkihas <ard00243@gmail.com>
2024-05-04 21:34:37 +0700

    Implement DxoMark get from scrape and convert to go Struct

--- model/dxomark.go
+++ model/dxomark.go
@@ -1,10 +1,27 @@
 package model
 
 type DxoMark struct {
-	device  string
-	camera  int
-	selfie  int
-	Audio   int
-	Display int
-	battery int
+	Name    string `json:"name"`
+	Camera  int    `json:"mobileScore"`
+	Selfie  int    `json:"selfieScore"`
+	Audio   int    `json:"audioScore"`
+	Display int    `json:"displayScore"`
+	Battery int    `json:"batteryScore"`
+	Mobile  Mobile `json:"mobile"`
+}
+
+type Mobile struct {
+	Subscores Subscores `json:"subscores"`
+}
+
+type Subscores struct {
+	Photo   int `json:"photo"`
+	Bokeh   int `json:"bokeh"`
+	Preview int `json:"preview"`
+	Zoom    int `json:"zoom"`
+	Video   int `json:"video"`
+}
+
+type DxoMarkRequest struct {
+	SortBy string
 }
