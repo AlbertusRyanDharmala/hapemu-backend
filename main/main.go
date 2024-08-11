a6f01b2af91925207ac39b5d92c803e0fba6f2db
Hunkihas <ard00243@gmail.com>
2024-08-11 15:35:16 +0700

    adjustment

--- main/main.go
+++ main/main.go
@@ -16,11 +16,12 @@ func main() {
 	cosineSimilarityService := cosine.NewCosineSimilarityService()
 	vectorGeneratorService := vector.NewCosineSimilarityService()
 	hapemuDatabaseAccessor := accessor.NewHapemuDatabaseAccessor()
+	emailService := service.NewEmailService(587, "smtp.gmail.com", "hapemu.id@gmail.com", "fdvtmvobhemhxvvi")
 	recommendationService := service.NewRecommendationService(cosineSimilarityService, vectorGeneratorService, hapemuDatabaseAccessor)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
-	http.HandleFunc("/send-email", service.EmailRecommendations)
+	http.HandleFunc("/send-email", emailService.EmailRecommendations)
 	http.HandleFunc("/get-recommendations", recommendationService.RecommendSmartphones)
 
 	handler := cors.Default().Handler(http.DefaultServeMux)
